7676dcefe31c6f4d844e6d5b66a61f10afcbb1da
Rokas01 <rokas.deveikis@gmail.com>
2016-12-05 00:51:01 +0200

    test

--- web/index.php
+++ web/index.php
@@ -19,17 +19,17 @@ $result = mysqli_query($mysqli, "SELECT * FROM CRUD ORDER BY id DESC"); // using
 
 	<tr bgcolor='#CCCCCC'>
 		<td>Name</td>
-		<td>Age</td>
-		<td>Email</td>
+		<td>Date</td>
+		<td>Address</td>
 		<td>Update</td>
 	</tr>
 	<?php 
 	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
 	while($res = mysqli_fetch_array($result)) { 		
 		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
+		echo "<td>".$res['Name']."</td>";
+		echo "<td>".$res['Date']."</td>";
+		echo "<td>".$res['Address']."</td>";	
 		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
 	}
 	?>
