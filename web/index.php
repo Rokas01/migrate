adfcb3d00c8384cf9ee3bf24475493db6d7f82a5
Rokas01 <rokas.deveikis@gmail.com>
2016-12-05 00:33:24 +0200

    test

--- web/index.php
+++ web/index.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT Name, Date, Address FROM CRUD";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["id"]. " - Name: " . $row["Name"]. " " . $row["Date"]. "<br>";
     }
 } else {
     echo "0 results";
